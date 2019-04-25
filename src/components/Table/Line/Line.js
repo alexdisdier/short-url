2dae901ef5e111bb9782ab9d56fe1c898e99fcaf
Alexandre Disdier <28678511+alexdisdier@users.noreply.github.com>
2019-04-25 16:13:41 +0200

    working on setting up apollo client into a stateless component

--- src/components/Table/Line/Line.js
+++ src/components/Table/Line/Line.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { gql } from "apollo-boost";
+import { graphql } from "react-apollo";
 // source: https://react-tooltip.netlify.com/
 import ReactTooltip from "react-tooltip";
 import domain from "../../../assets/domain";
@@ -6,9 +8,20 @@ import { ReactComponent as Copy } from "../../../assets/img/copy.svg";
 
 import "./Line.css";
 
+const getUrlsQuery = gql`
+  {
+    urls {
+      original
+      short
+      visits
+      id
+    }
+  }
+`;
+
 const line = props => {
   const { id, original, short, visits, incVisits, copyToClipboard } = props;
-
+  console.log(props);
   return (
     <ul className="table-row">
       <li>
@@ -38,4 +51,4 @@ const line = props => {
   );
 };
 
-export default line;
+export default graphql(getUrlsQuery)(line);
