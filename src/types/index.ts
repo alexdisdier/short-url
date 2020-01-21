01d6da894d752cf0dbb27cb19fd6164831498e95
Alex Disdier <28678511+alexdisdier@users.noreply.github.com>
2020-01-21 09:15:21 +0100

    clean comments

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,31 +4,3 @@ export interface Urls {
   short: string;
   visits: number;
 }
-
-// export enum KeyUsage {
-//   SIGN = 'SIGN',
-//   ENCRYPT = 'ENCRYPT'
-// }
-
-// export enum CodeChallengeMethod {
-//   S256 = 'S256',
-//   plain = 'plain'
-// }
-
-// export enum TokenType {
-//   token = "token",
-//   authCode = "authCode"
-// }
-
-// export enum ResponseType {
-//   none = "none",
-//   code = "code"
-// }
-
-// export interface OAuthPayload {
-//   responseType: ResponseType;
-//   clientId: number;
-//   redirectUri: string;
-//   codeChallenge?: string;
-//   codeChallengeMethod?: CodeChallengeMethod;
-// }
