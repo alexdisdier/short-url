54b8ecb06712422f125345c9c25b253526cbcfdc
Alex Disdier <28678511+alexdisdier@users.noreply.github.com>
2021-03-13 03:34:17 +0100

    Update domain.js

--- src/assets/domain.js
+++ src/assets/domain.js
@@ -1,6 +1,6 @@
 // const local = "http://localhost:3001/";
 // const api = "https://short-url-alex-disdier.herokuapp.com/";
-const api = 'https://ptitlink.herokuapp.com';
+const api = 'https://ptitlink.herokuapp.com/';
 
 const domain = api;
 
